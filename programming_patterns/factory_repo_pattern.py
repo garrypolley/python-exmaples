f23f45795f4edef3456dfb0e4a32d9c60dfe3a90
Garry Polley <garry.polley@unit21.ai>
2022-08-25 09:36:43 -0500

    Add more details and example on the banana factory

--- programming_patterns/factory_repo_pattern.py
+++ programming_patterns/factory_repo_pattern.py
@@ -7,6 +7,8 @@ does not care what repository is used. There will be an outside piece of data th
 In this case the caller wants to get Fruit but doesn't care if it's from a gas station or the grocery store.
 In the scenario of our get Fruit -- we'll let that outside data be "worker_preference"
 """
+
+
 class GetFruitRepository(abc.ABC):
     """
     Base class that ensures the implement a get_item method
@@ -18,37 +20,40 @@ class GetFruitRepository(abc.ABC):
 
 
 class GetFruitFromGasStation(GetFruitRepository):
-
     def get_fruit_by_name(self, name):
         return f"Gas Station: {name}"
 
 
 class GetFruitFromGroceryStore(GetFruitRepository):
-
     def get_fruit_by_name(self, name):
         return f"Grocery Store: {name}"
+
+
+class SomePreferences:
+    """
+    Used to illustrate some other storage/state can be used to drive
+    how the FruitFactory decides where to source Fruit.
+    """
+
+    worker_preference = "gas_station"
 
 
 class FruitFactory:
     """
     By changing the `worker_preference` on this class the underlying way to get a fruit changes
-
-    Raises:
-        ValueError: _description_
-
-    Returns:
-        _type_: _description_
     """
 
     worker_preference = "gas_station"
 
     @staticmethod
     def repo() -> GetFruitRepository:
-        if FruitFactory.worker_preference == 'gas_station':
+        if SomePreferences.worker_preference == "gas_station":
             return GetFruitFromGasStation()
-        elif FruitFactory.worker_preference == 'grocery':
+        elif SomePreferences.worker_preference == "grocery":
             return GetFruitFromGroceryStore()
-        raise ValueError(f"Not a valid worker_preference of {FruitFactory.worker_preference}")
+        raise ValueError(
+            f"Not a valid worker_preference of {SomePreferences.worker_preference}"
+        )
 
 
 class Bananas(FruitFactory):
@@ -59,17 +64,20 @@ class Bananas(FruitFactory):
     >>> bananas_instance = Bananas()
     >>> bananas_instance.get_all()
     'Gas Station: Banana'
-    >>> FruitFactory.worker_preference = 'grocery'
+    >>> SomePreferences.worker_preference = 'grocery'
     >>> bananas_instance.get_all()
     'Grocery Store: Banana'
-    >>> FruitFactory.worker_preference = 'gas_station'
+    >>> SomePreferences.worker_preference = 'gas_station'
     >>> bananas_instance.get_all()
     'Gas Station: Banana'
-    >>> FruitFactory.worker_preference = 'cause_an_error'
+    >>> SomePreferences.worker_preference = 'cause_an_error'
     >>> bananas_instance.get_all()
     Traceback (most recent call last):
     ...
     ValueError: Not a valid worker_preference of cause_an_error
+    >>> SomePreferences.worker_preference = 'gas_station'
+    >>> bananas_instance.repo().get_fruit_by_name('Banana')
+    'Gas Station: Banana'
     """
 
     def get_all(self):
@@ -78,4 +86,5 @@ class Bananas(FruitFactory):
 
 if __name__ == "__main__":
     import doctest
+
     doctest.testmod()
